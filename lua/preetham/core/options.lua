f396fcc17377baa905a9f9b5ac2635dbfcffd7fa
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 11:43:08 +0100

    Added string dash config

--- lua/preetham/core/options.lua
+++ lua/preetham/core/options.lua
@@ -31,3 +31,6 @@ opt.clipboard:append("unnamedplus") -- use system clipboard as default register
 -- split windows
 opt.splitright = true -- split vertical window to the right
 opt.splitbelow = true -- split horizontal window to the bottom
+
+-- String settings
+opt.iskeyword:append("-") -- consider string-string as whole word
