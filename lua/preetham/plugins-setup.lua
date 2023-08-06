db5c20b2892b0d46ab94741d35fcfd6e9f9132f9
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 13:52:06 +0100

    Added snippet engine plugins

--- lua/preetham/plugins-setup.lua
+++ lua/preetham/plugins-setup.lua
@@ -63,6 +63,11 @@ return packer.startup(function(use)
   use("hrsh7th/cmp-buffer") -- source for text in buffer
   use("hrsh7th/cmp-path") -- source for file system paths
 
+  -- snippets
+  use("L3MON4D3/LuaSnip") -- snippet engine
+  use("saadparwaiz1/cmp_luasnip") -- for autocompletion
+  use("rafamadriz/friendly-snippets") -- useful snippets
+
   if packer_bootstrap then
     require("packer").sync()
   end
