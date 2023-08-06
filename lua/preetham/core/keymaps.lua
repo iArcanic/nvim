a85b087e651d2f27e07f45fd9de4edc1903b576a
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 11:59:44 +0100

    Added use of "jk" keys to escape insert mode

--- lua/preetham/core/keymaps.lua
+++ lua/preetham/core/keymaps.lua
@@ -2,3 +2,10 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
+
+---------------------
+-- General Keymaps
+---------------------
+
+-- use jk to exit insert mode
+keymap.set("i", "jk", "<ESC>")
