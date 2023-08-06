f6c96fe3157e297c87d062fb8c7bfa77eac69bee
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 11:35:51 +0100

    Added cursor line

--- lua/preetham/core/options.lua
+++ lua/preetham/core/options.lua
@@ -15,3 +15,6 @@ opt.wrap = false -- disable line wrapping
 -- search settings
 opt.ignorecase = true -- ignore case when searching
 opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
+
+-- cursor line
+opt.cursorline = true -- highlight the current cursor line
