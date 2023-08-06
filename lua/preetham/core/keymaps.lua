dd296cd9796ca193dc971a511ea0b0f55b2c7eea
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 12:06:35 +0100

    Added tab management keymaps

--- lua/preetham/core/keymaps.lua
+++ lua/preetham/core/keymaps.lua
@@ -25,3 +25,9 @@ keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically
 keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally
 keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width & height
 keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window
+
+-- tab management
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
