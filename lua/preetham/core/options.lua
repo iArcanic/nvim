0b5c68a34f36abe1d4deb3ffd8c1d6067726c81c
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 11:34:56 +0100

    Added search settings

--- lua/preetham/core/options.lua
+++ lua/preetham/core/options.lua
@@ -11,3 +11,7 @@ opt.autoindent = true -- copy indent from current line when starting new one
 
 -- line wrapping
 opt.wrap = false -- disable line wrapping
+
+-- search settings
+opt.ignorecase = true -- ignore case when searching
+opt.smartcase = true -- if you include mixed case in your search, assumes you want case-sensitive
