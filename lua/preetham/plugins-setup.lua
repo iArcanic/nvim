003da7911c7ae48e3563e60d536a2ea5bdce3e85
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 12:30:41 +0100

    Added vim-ReplaceWithRegister plugin

--- lua/preetham/plugins-setup.lua
+++ lua/preetham/plugins-setup.lua
@@ -38,6 +38,7 @@ return packer.startup(function(use)
 
   -- essential plugins
   use("tpope/vim-surround") -- add, delete, change surroundings (it's awesome)
+  use("inkarkat/vim-ReplaceWithRegister") -- replace with register contents using motion (gr + motion)
 
   if packer_bootstrap then
     require("packer").sync()
