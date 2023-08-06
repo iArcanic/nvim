c79bc330f4b8a79c9957aed83e755df3f4dc40a0
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 11:32:42 +0100

    Added tabs and indentation

--- lua/preetham/core/options.lua
+++ lua/preetham/core/options.lua
@@ -2,3 +2,9 @@ local opt = vim.opt -- for conciseness
 
 -- line numbers
 opt.number = true
+
+-- tabs & indentation
+opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
+opt.shiftwidth = 2 -- 2 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy indent from current line when starting new one
