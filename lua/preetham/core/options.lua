9d3205d2acdb7c2f80a1dca1eb410c968bf97d2c
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 11:33:47 +0100

    Disabled line wrapping

--- lua/preetham/core/options.lua
+++ lua/preetham/core/options.lua
@@ -8,3 +8,6 @@ opt.tabstop = 2 -- 2 spaces for tabs (prettier default)
 opt.shiftwidth = 2 -- 2 spaces for indent width
 opt.expandtab = true -- expand tab to spaces
 opt.autoindent = true -- copy indent from current line when starting new one
+
+-- line wrapping
+opt.wrap = false -- disable line wrapping
