1eaaa3e206a7dc5d5a5448415bbc75406662591c
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 12:04:11 +0100

    Added using leader + "+" and "-" keys to increment and decrement numbers

--- lua/preetham/core/keymaps.lua
+++ lua/preetham/core/keymaps.lua
@@ -15,3 +15,7 @@ keymap.set("n", "<leader>nh", ":nohl<CR>")
 
 -- delete single character without copying into register
 keymap.set("n", "x", '"_x')
+
+-- increment/decrement numbers
+keymap.set("n", "<leader>+", "<C-a>") -- increment
+keymap.set("n", "<leader>-", "<C-x>") -- decrement
