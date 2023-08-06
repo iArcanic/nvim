b5b7885904bfec0682cc8297f60251cbd68e47ca
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-06 12:01:02 +0100

    Added using leader + "nh" keys to clear all search highlights

--- lua/preetham/core/keymaps.lua
+++ lua/preetham/core/keymaps.lua
@@ -9,3 +9,6 @@ local keymap = vim.keymap -- for conciseness
 
 -- use jk to exit insert mode
 keymap.set("i", "jk", "<ESC>")
+
+-- clear search highlights
+keymap.set("n", "<leader>nh", ":nohl<CR>")
