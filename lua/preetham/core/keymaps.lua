e23108ff7094023a19075f09b623d580f3cdf3e2
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-07 12:32:16 +0100

    Added keybinds for nvim-tree

--- lua/preetham/core/keymaps.lua
+++ lua/preetham/core/keymaps.lua
@@ -38,3 +38,6 @@ keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
 -- vim-maximizer
 keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
+
+-- nvim-tree
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>") -- toggle file explorer
