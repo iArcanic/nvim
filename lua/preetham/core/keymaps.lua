e75ec6679cc0eafaf1d49ea7bb16ed2f043102a8
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-07 12:29:07 +0100

    Added keybinds for split window maximization

--- lua/preetham/core/keymaps.lua
+++ lua/preetham/core/keymaps.lua
@@ -31,3 +31,10 @@ keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
 keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
+
+----------------------
+-- Plugin Keybinds
+----------------------
+
+-- vim-maximizer
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>") -- toggle split window maximization
