fde95ddb52bbc69aeafe7d21348617406e697ae4
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-08-07 10:54:27 +0100

    Added LSP plugins

--- lua/preetham/plugins-setup.lua
+++ lua/preetham/plugins-setup.lua
@@ -68,6 +68,10 @@ return packer.startup(function(use)
   use("saadparwaiz1/cmp_luasnip") -- for autocompletion
   use("rafamadriz/friendly-snippets") -- useful snippets
 
+  -- managing & installing lsp servers, linters & formatters
+  use("williamboman/mason.nvim") -- in charge of managing lsp servers, linters & formatters
+  use("williamboman/mason-lspconfig.nvim") -- bridges gap b/w mason & lspconfig
+
   if packer_bootstrap then
     require("packer").sync()
   end
